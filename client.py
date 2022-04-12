7b75ae121bb49a14003889528d6b5f363fdce28c
Pietro Squilla <34872598+ScratchyCode@users.noreply.github.com>
2022-04-12 14:41:32 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,30 +1,31 @@
 import socket
+import hashlib
 
+host = input("Remote host: ")
+port = int(input("Port: "))
 
-
-# create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# connect the client
-# client.connect((target, port))
-client.connect(('127.0.0.1', 1233))
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect((host,port))
 response = client.recv(2048)
-# Input UserName
-name = input(response.decode())	
-client.send(str.encode(name))
+
+# input username
+user = input(response.decode())	
+client.send(str.encode(user))
 response = client.recv(2048)
-# Input Password
-password = input(response.decode())	
-client.send(str.encode(password))
-''' Response : Status of Connection :
-	1 : Registeration successful 
-	2 : Connection Successful
-	3 : Login Failed
-'''
-# Receive response 
+
+# input password
+password = input(response.decode())
+password = hashlib.sha256(str.encode(password)).hexdigest()
+client.send(str.encode(password)) # send only hashed passwd over network
+
+#response: status of connection
+#1: registeration successful
+#2: connection successful
+#3: login failed
+
+# receive response 
 response = client.recv(2048)
 response = response.decode()
-
 print(response)
-client.close()
 
+client.close()
