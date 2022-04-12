07d8140b1f92a2ea730377efe0ecf94b36c293d7
Pietro Squilla <34872598+ScratchyCode@users.noreply.github.com>
2022-04-12 14:42:08 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,61 +3,63 @@ import os
 import threading
 import hashlib
 
+# create socket (TCP) connection
+serverSocket = socket.socket(family = socket.AF_INET,type = socket.SOCK_STREAM) 
+#host = "127.0.0.1"
+host = ""
+port = int(input("Listening port: "))
+threadCount = 0
 
-# Create Socket (TCP) Connection
-ServerSocket = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM) 
-host = '127.0.0.1'
-port = 1233
-ThreadCount = 0
 try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
+    serverSocket.bind((host,port))
+except socket.error as errore:
+    print(str(errore))
 
-print('Waitiing for a Connection..')
-ServerSocket.listen(5)
+print("Waiting for a connection...")
+serverSocket.listen(5)
 HashTable = {}
 
-# Function : For each client 
+# Function for each client 
 def threaded_client(connection):
-    connection.send(str.encode('ENTER USERNAME : ')) # Request Username
+    connection.send(str.encode("Enter username: ")) # request username
     name = connection.recv(2048)
-    connection.send(str.encode('ENTER PASSWORD : ')) # Request Password
+    connection.send(str.encode("Enter password: ")) # request password
     password = connection.recv(2048)
     password = password.decode()
     name = name.decode()
-    password=hashlib.sha256(str.encode(password)).hexdigest() # Password hash using SHA256
-# REGISTERATION PHASE   
-# If new user,  regiter in Hashtable Dictionary  
+    
+# REGISTERATION PHASE
+    # if new user, regiter in hashtable dictionary
     if name not in HashTable:
         HashTable[name]=password
-        connection.send(str.encode('Registeration Successful')) 
-        print('Registered : ',name)
-        print("{:<8} {:<20}".format('USER','PASSWORD'))
+        connection.send(str.encode("Registeration successful")) 
+        print("Registered: ",name)
+        print("{:<8} {:<20}".format("USER","PASSWORD"))
         for k, v in HashTable.items():
             label, num = k,v
             print("{:<8} {:<20}".format(label, num))
         print("-------------------------------------------")
-        
+    
     else:
-# If already existing user, check if the entered password is correct
+        # if already existing user, check if the entered password is correct
         if(HashTable[name] == password):
-            connection.send(str.encode('Connection Successful')) # Response Code for Connected Client 
-            print('Connected : ',name)
+            connection.send(str.encode("Connection successful")) # response code for connected client
+            print("Connected : ",name)
         else:
-            connection.send(str.encode('Login Failed')) # Response code for login failed
-            print('Connection denied : ',name)
+            connection.send(str.encode("Login failed")) # response code for login failed
+            print("Connection denied: ",name)
     while True:
         break
     connection.close()
 
+##############
+#    main    #
+##############
 while True:
-    Client, address = ServerSocket.accept()
-    client_handler = threading.Thread(
-        target=threaded_client,
-        args=(Client,)  
-    )
+    client,address = serverSocket.accept()
+    client_handler = threading.Thread(target=threaded_client,args=(client, ))
     client_handler.start()
-    ThreadCount += 1
-    print('Connection Request: ' + str(ThreadCount))
-ServerSocket.close()
+    threadCount += 1
+    print("Connection request: " + str(threadCount))
+
+serverSocket.close()
